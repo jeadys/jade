bc1ab8f6b6fa41f51c0de9e306d27fc2a516d2d6
Jeadys <yassin.ao.work@gmail.com>
2024-03-14 00:05:47 +0100

    Rework leg class to be more scalable

--- joints/leg.py
+++ joints/leg.py
@@ -1,125 +1,87 @@
 import maya.cmds as cmds
 from importlib import reload
 
-import mechanisms.leg_roll as leg_roll_module
+import controllers.control_shape as control_shape
+from mechanisms.leg_roll import LegRoll
+from typing import Literal
+from dataclasses import dataclass
 
-reload(leg_roll_module)
+reload(control_shape)
+
+
+@dataclass(frozen=True)
+class LegSegment:
+    name: str
+    position: tuple[float, float, float]
 
 
 class Leg:
-    def __init__(self, prefix):
+    locator_parent_group = "locators"
+    joint_parent_group = "skeleton"
+
+    def __init__(self, prefix: Literal["L", "R"]):
         self.prefix = prefix
+        self.multiplier = 1 if self.prefix == "L" else -1
+        self.leg_segments = (
+            LegSegment(name="upperleg", position=(self.multiplier * 15, 145, 0)),
+            LegSegment(name="lowerleg", position=(self.multiplier * 20, 80, 0)),
+            LegSegment(name="ankle", position=(self.multiplier * 25, 15, -10)),
+        )
+        self.leg_roll: LegRoll = LegRoll(prefix=self.prefix)
 
-    def create_leg_locators(self):
-        if cmds.objExists(f"{self.prefix}_LOC_leg_group"):
-            return
+    def create_leg_locators(self) -> None:
+        leg_locators_exist: bool = any(cmds.objExists(f"{self.prefix}_LOC_{locator}") for locator in self.leg_segments)
 
-        cmds.group(empty=True, name=f"{self.prefix}_LOC_leg_group")
+        if leg_locators_exist:
+            cmds.error(f"{self.prefix} leg locators already exist in {self.locator_parent_group}.")
 
-        if not cmds.objExists("locators"):
-            cmds.group(empty=True, name="locators")
+        if not cmds.objExists(self.locator_parent_group):
+            cmds.group(empty=True, name=self.locator_parent_group)
 
-        cmds.parent(f"{self.prefix}_LOC_leg_group", "locators")
+        previous_locator = self.locator_parent_group
+        for index, locator in enumerate(self.leg_segments):
+            current_locator = cmds.spaceLocator(name=f"{self.prefix}_LOC_{locator.name}")
+            cmds.scale(2, 2, 2, current_locator)
+            cmds.move(*locator.position, current_locator)
+            cmds.parent(current_locator, previous_locator)
 
-        # Default start positions of the leg locators
-        if self.prefix == "L":
-            position_femur = (15, 145, 0)
-            position_tibia = (20, 80, 0)
-            position_ankle = (25, 15, -10)
-            position_ball = (25, 2.5, 0)
-            position_ball_end = (25, 2.5, 20)
-        else:
-            position_femur = (-15, 145, 0)
-            position_tibia = (-20, 80, 0)
-            position_ankle = (-25, 15, -10)
-            position_ball = (-25, 2.5, 0)
-            position_ball_end = (-25, 2.5, 20)
+            previous_locator = current_locator
 
-        # femur
-        loc_femur = cmds.spaceLocator(name=f"{self.prefix}_LOC_femur")
-        cmds.scale(2, 2, 2, loc_femur)
-        cmds.move(*position_femur, loc_femur)
-        cmds.parent(loc_femur, f"{self.prefix}_LOC_leg_group")
+    def create_leg_joints(self, is_auto_parent: bool, is_roll_limb: bool) -> None:
+        leg_joints_exist: bool = any(cmds.objExists(f"{self.prefix}_DEF_{joint}") for joint in self.leg_segments)
 
-        # tibia
-        loc_tibia = cmds.spaceLocator(name=f"{self.prefix}_LOC_tibia")
-        cmds.scale(2, 2, 2, loc_tibia)
-        cmds.move(*position_tibia, loc_tibia)
-        cmds.parent(loc_tibia, loc_femur)
+        if leg_joints_exist:
+            cmds.error(f"{self.prefix} leg joints already exist in {self.joint_parent_group}")
 
-        # ankle
-        loc_ankle = cmds.spaceLocator(name=f"{self.prefix}_LOC_ankle")
-        cmds.scale(2, 2, 2, loc_ankle)
-        cmds.move(*position_ankle, loc_ankle)
-        cmds.parent(loc_ankle, loc_tibia)
+        if not cmds.objExists(self.joint_parent_group):
+            cmds.group(empty=True, name=self.joint_parent_group)
 
-        # ball
-        loc_ball = cmds.spaceLocator(name=f"{self.prefix}_LOC_ball")
-        cmds.scale(2, 2, 2, loc_ball)
-        cmds.move(*position_ball, loc_ball)
-        cmds.parent(loc_ball, loc_ankle)
+        previous_joint = self.joint_parent_group
+        for index, joint in enumerate(self.leg_segments):
+            # Prevent Maya from auto parenting joint to selected item in scene.
+            cmds.select(deselect=True)
+            current_joint = cmds.joint(radius=3, rotationOrder="zxy", name=f"{self.prefix}_DEF_{joint.name}")
+            cmds.matchTransform(current_joint, f"{self.prefix}_LOC_{joint.name}", position=True, rotation=True, scale=False)
+            cmds.parent(current_joint, previous_joint)
 
-        # ball end
-        loc_ball_end = cmds.spaceLocator(name=f"{self.prefix}_LOC_ball_end")
-        cmds.scale(2, 2, 2, loc_ball_end)
-        cmds.move(*position_ball_end, loc_ball_end)
-        cmds.parent(loc_ball_end, loc_ball)
+            previous_joint = current_joint
 
-    def create_leg_joints(self, is_auto_parent: bool, is_roll_limb: bool):
-        if cmds.objExists(f"{self.prefix}_DEF_femur") and cmds.objExists(f"{self.prefix}_DEF_tibia") and cmds.objExists(
-                f"{self.prefix}_DEF_ankle") and cmds.objExists(f"{self.prefix}_DEF_ball") and cmds.objExists(
-                f"{self.prefix}_DEF_ball_end") or not cmds.objExists(f"{self.prefix}_LOC_leg_group"):
-            return
+        for index, joint in enumerate(self.leg_segments):
+            if index == len(self.leg_segments) - 1:
+                cmds.joint(f"{self.prefix}_DEF_{joint.name}", edit=True, orientJoint="none", zeroScaleOrient=True)
+            else:
+                cmds.joint(f"{self.prefix}_DEF_{joint.name}", edit=True, orientJoint="yzx", secondaryAxisOrient="zup",
+                           zeroScaleOrient=True)
 
-        if not cmds.objExists("skeleton"):
-            cmds.group(empty=True, name="skeleton")
-
-        # Prevent Maya from auto parenting elements to newly created group
-        cmds.select(deselect=True)
-
-        # femur
-        def_femur = cmds.joint(radius=3, rotationOrder="zxy", name=f"{self.prefix}_DEF_femur")
-        cmds.matchTransform(def_femur, f"{self.prefix}_LOC_femur", position=True, rotation=True, scale=False)
-
-        # tibia
-        def_tibia = cmds.joint(radius=3, rotationOrder="zxy", name=f"{self.prefix}_DEF_tibia")
-        cmds.matchTransform(def_tibia, f"{self.prefix}_LOC_tibia", position=True, rotation=True, scale=False)
-
-        # ankle
-        def_ankle = cmds.joint(radius=3, rotationOrder="zxy", name=f"{self.prefix}_DEF_ankle")
-        cmds.matchTransform(def_ankle, f"{self.prefix}_LOC_ankle", position=True, rotation=True, scale=False)
-
-        # ball
-        def_ball = cmds.joint(radius=3, rotationOrder="zxy", name=f"{self.prefix}_DEF_ball")
-        cmds.matchTransform(def_ball, f"{self.prefix}_LOC_ball", position=True, rotation=True, scale=False)
-
-        # ball end
-        def_ball_end = cmds.joint(radius=3, rotationOrder="zxy", name=f"{self.prefix}_DEF_ball_end")
-        cmds.matchTransform(def_ball_end, f"{self.prefix}_LOC_ball_end", position=True, rotation=True, scale=False)
-
-        # Parent joint chain
-        cmds.parent(def_femur, "skeleton")
-
-        # orient leg joints
-        cmds.joint(f"{self.prefix}_DEF_femur", edit=True, orientJoint="yzx", secondaryAxisOrient="zup",
-                   zeroScaleOrient=True)
-        cmds.joint(f"{self.prefix}_DEF_tibia", edit=True, orientJoint="yzx", secondaryAxisOrient="zup",
-                   zeroScaleOrient=True)
-        cmds.joint(f"{self.prefix}_DEF_ball", edit=True, orientJoint="yzx", secondaryAxisOrient="zup",
-                   zeroScaleOrient=True)
-        cmds.joint(f"{self.prefix}_DEF_ball_end", edit=True, orientJoint="none", zeroScaleOrient=True)
-
-        if is_auto_parent and cmds.objExists("DEF_cog"):
-            cmds.parent(def_femur, "DEF_cog")
+        if is_auto_parent and cmds.objExists("C_DEF_cog"):
+            cmds.parent(f"{self.prefix}_DEF_upperleg", "C_DEF_cog")
 
         if is_roll_limb:
-            leg_roll_instance = leg_roll_module.LegRoll(prefix=self.prefix)
-            leg_roll_instance.create_leg_roll_locators()
-            leg_roll_instance.create_leg_roll_joints()
-            leg_roll_instance.create_leg_roll_handles()
-            leg_roll_instance.create_leg_roll_constraints()
-            leg_roll_instance.create_leg_roll_hierarchy()
-            leg_roll_instance.create_leg_roll_nodes()
+            self.leg_roll.create_leg_roll_locators()
+            self.leg_roll.create_leg_roll_joints()
+            self.leg_roll.create_leg_roll_handles()
+            self.leg_roll.create_leg_roll_constraints()
+            self.leg_roll.create_leg_roll_nodes()
 
 
 if __name__ == "__main__":
