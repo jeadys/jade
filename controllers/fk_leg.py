4d0066ea076a818a9dd33bec6ce6677fd9d768a7
Jeadys <yassin.ao.work@gmail.com>
2024-03-14 00:06:14 +0100

    Rework fk leg class to be more scalable

--- controllers/fk_leg.py
+++ controllers/fk_leg.py
@@ -1,69 +1,69 @@
 import maya.cmds as cmds
+import mechanisms.leg_stretch as leg_stretch_module
+from joints.arm_bind import bake_transform_to_offset_parent_matrix
 from controllers.control_shape import ControlShape
 from importlib import reload
 import controllers.control_shape as control_shape
+from utilities.enums import RotateOrder
+from dataclasses import dataclass
 
 reload(control_shape)
+reload(leg_stretch_module)
+
+
+@dataclass(frozen=True)
+class LegSegment:
+    name: str
+    shape: str
 
 
 class FKLeg:
     def __init__(self, prefix) -> None:
         self.prefix = prefix
+        self.leg_segments = (
+            LegSegment(name="upperleg", shape="circle"),
+            LegSegment(name="lowerleg", shape="circle"),
+            LegSegment(name="ankle", shape="star"),
+        )
 
-    def create_fk_arm_control(self, control_name: str, connected_limb: str):
-        fk_offset = cmds.group(empty=True, name=f"{self.prefix}_FK_OFFSET_{control_name}")
+        self.kinematic_parent_group = f"{self.prefix}_leg_kinematics"
+        self.control_parent_group = f"{self.prefix}_leg_controls"
+        self.control_shape: ControlShape = ControlShape()
 
-        fk_ctrl = ControlShape.curve_circle(name=f"{self.prefix}_FK_CTRL_{control_name}")[0]
+    def create_fk_leg_joints(self):
+        if not cmds.objExists(self.kinematic_parent_group):
+            cmds.group(empty=True, name=self.kinematic_parent_group)
+            cmds.parent(self.kinematic_parent_group, "rig_systems")
 
-        cmds.setAttr(f"{fk_ctrl}.overrideEnabled", 1)
-        cmds.setAttr(f"{fk_ctrl}.overrideColor", 12)
+        previous_fk_joint = self.kinematic_parent_group
+        for index, joint in enumerate(self.leg_segments):
+            current_fk_joint = cmds.duplicate(f"{self.prefix}_DEF_{joint.name}", parentOnly=True, name=f"{self.prefix}_FK_{joint.name}")[0]
+            cmds.parentConstraint(f"{self.prefix}_FK_{joint.name}", f"{self.prefix}_DEF_{joint.name}", maintainOffset=True)
+            cmds.parent(current_fk_joint, previous_fk_joint)
 
-        cmds.parent(fk_ctrl, fk_offset)
+            bake_transform_to_offset_parent_matrix(current_fk_joint)
 
-        cmds.matchTransform(fk_offset, f"{self.prefix}_DEF_{connected_limb}", position=True, rotation=True, scale=False)
+            previous_fk_joint = current_fk_joint
 
-        self.create_fk_arm_constraints(fk_ctrl=fk_ctrl, connected_limb=connected_limb)
+    def create_fk_leg_controls(self):
+        if not cmds.objExists(self.control_parent_group):
+            cmds.group(empty=True, name=self.control_parent_group)
+            cmds.parent(self.control_parent_group, "controls")
 
-        return fk_offset, fk_ctrl
+        previous_fk_control = self.control_parent_group
+        for index, joint in enumerate(self.leg_segments):
+            current_fk_ctrl = self.control_shape.select_control_shape(shape=joint.shape, name=f"{self.prefix}_FK_CTRL_{joint.name}")
+            cmds.setAttr(f"{current_fk_ctrl}.rotateOrder", RotateOrder.ZXY.value)
+            cmds.matchTransform(current_fk_ctrl, f"{self.prefix}_DEF_{joint.name}", position=True, rotation=True, scale=False)
+            cmds.parent(current_fk_ctrl, previous_fk_control)
 
-    def create_fk_arm_joints(self):
-        if not cmds.objExists(f"{self.prefix}_leg_group"):
-            cmds.group(empty=True, name=f"{self.prefix}_leg_group")
-            cmds.parent(f"{self.prefix}_leg_group", "rig_systems")
+            if index == 0:
+                cmds.connectAttr(f"{self.prefix}_FK_CTRL_{joint.name}.worldMatrix[0]",
+                                 f"{self.prefix}_FK_{joint.name}.offsetParentMatrix")
+            else:
+                cmds.connectAttr(f"{self.prefix}_FK_CTRL_{joint.name}.translate", f"{self.prefix}_FK_{joint.name}.translate")
+                cmds.connectAttr(f"{self.prefix}_FK_CTRL_{joint.name}.rotate", f"{self.prefix}_FK_{joint.name}.rotate")
 
-        fk_femur = cmds.duplicate(f"{self.prefix}_DEF_femur", parentOnly=True, name=f"{self.prefix}_FK_femur")
-        fk_tibia = cmds.duplicate(f"{self.prefix}_DEF_tibia", parentOnly=True, name=f"{self.prefix}_FK_tibia")
-        fk_ankle = cmds.duplicate(f"{self.prefix}_DEF_ankle", parentOnly=True, name=f"{self.prefix}_FK_ankle")
-        fk_ball = cmds.duplicate(f"{self.prefix}_DEF_ball", parentOnly=True, name=f"{self.prefix}_FK_ball")
-        fk_ball_end = cmds.duplicate(f"{self.prefix}_DEF_ball_end", parentOnly=True, name=f"{self.prefix}_FK_ball_end")
+            bake_transform_to_offset_parent_matrix(current_fk_ctrl)
 
-        cmds.parent(fk_femur, f"{self.prefix}_leg_group")
-        cmds.parent(fk_tibia, fk_femur)
-        cmds.parent(fk_ankle, fk_tibia)
-        cmds.parent(fk_ball, fk_ankle)
-        cmds.parent(fk_ball_end, fk_ball)
-
-    def create_fk_arm_controls(self):
-        self.create_fk_arm_joints()
-
-        fk_offset_femur, fk_ctrl_femur = self.create_fk_arm_control(control_name="femur", connected_limb="femur")
-        fk_offset_tibia, fk_ctrl_tibia = self.create_fk_arm_control(control_name="tibia", connected_limb="tibia")
-        fk_offset_ankle, fk_ctrl_ankle = self.create_fk_arm_control(control_name="ankle", connected_limb="ankle")
-        fk_offset_ball, fk_ctrl_ball = self.create_fk_arm_control(control_name="ball", connected_limb="ball")
-        fk_offset_ball_end, fk_ctrl_ball_end = self.create_fk_arm_control(control_name="ball_end",
-                                                                          connected_limb="ball_end")
-
-        cmds.parent(fk_offset_femur, f"{self.prefix}_leg_controls")
-        cmds.parent(fk_offset_tibia, fk_ctrl_femur)
-        cmds.parent(fk_offset_ankle, fk_ctrl_tibia)
-        cmds.parent(fk_offset_ball, fk_ctrl_ankle)
-        cmds.parent(fk_offset_ball_end, fk_ctrl_ball)
-
-    def create_fk_arm_constraints(self, fk_ctrl, connected_limb) -> None:
-        cmds.parentConstraint(fk_ctrl, f"{self.prefix}_FK_{connected_limb}", maintainOffset=True)
-        cmds.parentConstraint(f"{self.prefix}_FK_{connected_limb}", f"{self.prefix}_DEF_{connected_limb}",
-                              maintainOffset=True)
-
-
-if __name__ == "__main__":
-    pass
+            previous_fk_control = current_fk_ctrl
