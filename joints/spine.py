dc92780e5f3128740f54adb080c317d5d01020d3
Jeadys <yassin.ao.work@gmail.com>
2024-03-22 23:07:17 +0100

    Rename spine modules

--- joints/spine.py
+++ joints/spine.py
@@ -1,124 +1,83 @@
 import maya.cmds as cmds
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class SpineSegment:
+    name: str
+    position: tuple[float, float, float]
 
 
 class Spine:
-    def __init__(self):
-        pass
+    locator_parent_group = "locators"
+    joint_parent_group = "skeleton"
 
-    @staticmethod
-    def create_spine_locators():
-        if cmds.objExists(f"LOC_spine_group"):
-            return
-        else:
-            cmds.group(empty=True, name=f"LOC_spine_group")
+    def __init__(self, prefix, spine_count):
+        self.prefix = prefix
+        self.spine_count = spine_count
+        self.distance_between_spines = 60 / (self.spine_count - 1)  # Adjust for spacing between spines
+        self.spine_segments = tuple(
+            SpineSegment(name=f"{self.prefix}_spine_0{spine + 1}",
+                         position=(0, 150 + (self.distance_between_spines * spine), 0))
+            for spine in range(self.spine_count)
+        )
 
-        if cmds.objExists("locators"):
-            cmds.parent(f"LOC_spine_group", "locators")
-        else:
-            cmds.group(empty=True, name="locators")
-            cmds.parent(f"LOC_spine_group", "locators")
+    def create_spine_locators(self):
+        spine_locators_exist: bool = any(cmds.objExists(f"{locator.name}_LOC") for locator in self.spine_segments)
 
-        # root
-        loc_root = cmds.spaceLocator(name="LOC_root")
-        cmds.scale(2, 2, 2, loc_root)
-        cmds.move(0, 0, 0, loc_root)
-        cmds.parent(loc_root, "LOC_spine_group")
+        if spine_locators_exist:
+            cmds.error(f"C spine locators already exist in {self.locator_parent_group}.")
 
-        # cog
-        loc_cog = cmds.spaceLocator(name="LOC_cog")
-        cmds.scale(2, 2, 2, loc_cog)
-        cmds.move(0, 150, 0, loc_cog)
-        cmds.parent(loc_cog, loc_root)
+        if not cmds.objExists(self.locator_parent_group):
+            cmds.group(empty=True, name=self.locator_parent_group)
 
-        # pelvis
-        loc_pelvis = cmds.spaceLocator(name="LOC_pelvis")
-        cmds.scale(2, 2, 2, loc_pelvis)
-        cmds.move(0, 150, 0, loc_pelvis)
-        cmds.parent(loc_pelvis, loc_cog)
+        previous_locator = self.locator_parent_group
+        for index, locator in enumerate(self.spine_segments):
+            current_locator = cmds.spaceLocator(name=f"{locator.name}_LOC")[0]
+            cmds.setAttr(f"{current_locator}.overrideEnabled", True)
+            cmds.setAttr(f"{current_locator}.overrideRGBColors", True)
+            cmds.setAttr(f"{current_locator}.overrideColorRGB", 1, 1, 0)
 
-        # spine one
-        loc_spine_one = cmds.spaceLocator(name="LOC_spine_01")
-        cmds.scale(2, 2, 2, loc_spine_one)
-        cmds.move(0, 170, 5, loc_spine_one)
-        cmds.parent(loc_spine_one, loc_pelvis)
+            cmds.scale(2, 2, 2, current_locator)
+            cmds.move(*locator.position, current_locator)
+            cmds.parent(current_locator, previous_locator)
 
-        # spine two
-        loc_spine_two = cmds.spaceLocator(name="LOC_spine_02")
-        cmds.scale(2, 2, 2, loc_spine_two)
-        cmds.move(0, 190, 7.5, loc_spine_two)
-        cmds.parent(loc_spine_two, loc_spine_one)
+            previous_locator = current_locator
 
-        # spine three
-        loc_spine_three = cmds.spaceLocator(name="LOC_spine_03")
-        cmds.scale(2, 2, 2, loc_spine_three)
-        cmds.move(0, 210, 5, loc_spine_three)
-        cmds.parent(loc_spine_three, loc_spine_two)
+    def create_spine_joints(self):
+        spine_joints_exist: bool = any(cmds.objExists(f"{joint.name}") for joint in self.spine_segments)
 
-        # neck
-        loc_neck = cmds.spaceLocator(name="LOC_neck")
-        cmds.scale(2, 2, 2, loc_neck)
-        cmds.move(0, 230, 0, loc_neck)
-        cmds.parent(loc_neck, loc_spine_three)
+        if spine_joints_exist:
+            cmds.error(f"C spine joints already exist in {self.joint_parent_group}")
 
-    @staticmethod
-    def create_spine_joints(is_auto_parent: bool):
-        if not cmds.objExists("LOC_spine_group"):
-            return
+        if not cmds.objExists(self.joint_parent_group):
+            cmds.group(empty=True, name=self.joint_parent_group)
 
-        if not cmds.objExists("skeleton"):
-            cmds.group(empty=True, name="skeleton")
+        previous_joint = self.joint_parent_group
+        for index, joint in enumerate(self.spine_segments):
+            # Prevent Maya from auto parenting joint to selected item in scene.
+            cmds.select(deselect=True)
+            current_joint = cmds.joint(radius=3, rotationOrder="zxy", name=f"{joint.name}")
+            cmds.matchTransform(current_joint, f"{joint.name}_LOC", position=True, rotation=True, scale=False)
+            cmds.parent(current_joint, previous_joint)
 
-        # Prevent Maya from auto parenting elements to newly created group
-        cmds.select(deselect=True)
+            previous_joint = current_joint
 
-        # root
-        def_root = cmds.joint(radius=3, rotationOrder="zxy", name="DEF_root")
-        cmds.matchTransform(def_root, "LOC_root", position=True, rotation=True, scale=False)
+        for index, joint in enumerate(self.spine_segments):
+            if index == len(self.spine_segments) - 1:
+                cmds.joint(f"{joint.name}", edit=True, orientJoint="none", zeroScaleOrient=True)
+            else:
+                cmds.joint(f"{joint.name}", edit=True, orientJoint="yzx", secondaryAxisOrient="zup",
+                           zeroScaleOrient=True)
 
-        # cog
-        def_cog = cmds.joint(radius=3, rotationOrder="zxy", name="DEF_cog")
-        cmds.matchTransform(def_cog, "LOC_cog", position=True, rotation=True, scale=False)
-
-        # pelvis
-        def_pelvis = cmds.joint(radius=3, rotationOrder="zxy", name="DEF_pelvis")
-        cmds.matchTransform(def_pelvis, "LOC_pelvis", position=True, rotation=True, scale=False)
-
-        # spine one
-        def_spine_one = cmds.joint(radius=3, rotationOrder="zxy", name="DEF_spine_01")
-        cmds.matchTransform(def_spine_one, "LOC_spine_01", position=True, rotation=True, scale=False)
-
-        # spine two
-        def_spine_two = cmds.joint(radius=3,  rotationOrder="zxy", name="DEF_spine_02")
-        cmds.matchTransform(def_spine_two, "LOC_spine_02", position=True, rotation=True, scale=False)
-
-        # spine three
-        def_spine_three = cmds.joint(radius=3, rotationOrder="zxy", name="DEF_spine_03")
-        cmds.matchTransform(def_spine_three, "LOC_spine_03", position=True, rotation=True, scale=False)
-
-        # neck
-        def_neck = cmds.joint(radius=3, rotationOrder="zxy", name="DEF_neck")
-        cmds.matchTransform(def_neck, "LOC_neck", position=True, rotation=False, scale=False)
-
-        # Parent joint chain
-        cmds.parent(def_root, "skeleton")
-
-        # Orient spine joints
-        cmds.joint("DEF_root", edit=True, orientJoint="none", zeroScaleOrient=True)
-        cmds.joint("DEF_cog", edit=True, orientJoint="yzx", secondaryAxisOrient="zup", zeroScaleOrient=True)
-        cmds.joint("DEF_pelvis", edit=True, orientJoint="yzx", secondaryAxisOrient="zup", zeroScaleOrient=True)
-        cmds.joint("DEF_spine_01", edit=True, orientJoint="yzx", secondaryAxisOrient="zup", zeroScaleOrient=True)
-        cmds.joint("DEF_spine_02", edit=True, orientJoint="yzx", secondaryAxisOrient="zup", zeroScaleOrient=True)
-        cmds.joint("DEF_spine_03", edit=True, orientJoint="yzx", secondaryAxisOrient="zup", zeroScaleOrient=True)
-        cmds.joint("DEF_neck", edit=True, orientJoint="none", zeroScaleOrient=True)
-
-        if is_auto_parent and cmds.objExists("L_DEF_clavicle"):
-            cmds.parent("L_DEF_clavicle", def_spine_three)
-        if is_auto_parent and cmds.objExists("R_DEF_clavicle"):
-            cmds.parent("R_DEF_clavicle", def_spine_three)
-        if is_auto_parent and cmds.objExists("L_DEF_femur"):
-            cmds.parent("L_DEF_femur", def_cog)
-        if is_auto_parent and cmds.objExists("R_DEF_femur"):
-            cmds.parent("R_DEF_femur", def_cog)
+        # if is_auto_parent and cmds.objExists("L_clavicle"):
+        #     cmds.parent("L_clavicle", "C_spine_03")
+        # if is_auto_parent and cmds.objExists("R_clavicle"):
+        #     cmds.parent("R_clavicle", "C_spine_03")
+        # if is_auto_parent and cmds.objExists("L_upperleg"):
+        #     cmds.parent(f"L_upperleg", "C_pelvis")
+        # if is_auto_parent and cmds.objExists("R_upperleg"):
+        #     cmds.parent(f"R_upperleg", "C_pelvis")
 
 
 if __name__ == "__main__":
