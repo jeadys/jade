05ad1c6f4b4d0cdd8107180ad3b64e0e95512cce
Jeadys <yassin.ao.work@gmail.com>
2024-04-11 22:48:40 +0200

    Add unload package function to update changes to Maya directly without having to reload or restart

---
+++ utilities/unload_packages.py
@@ -0,0 +1,27 @@
+import sys
+
+# if you have some packages that you often reload, you can put them here.
+# And they will get reloaded if "packages" attribute is not explicitly stated
+DEFAULT_RELOAD_PACKAGES = []
+
+
+def unload_packages(silent=True, packages=None):
+    if packages is None:
+        packages = DEFAULT_RELOAD_PACKAGES
+
+    # construct reload list
+    reload_list = []
+    for i in sys.modules.keys():
+        for package in packages:
+            if i.startswith(package):
+                reload_list.append(i)
+
+    # unload everything
+    for i in reload_list:
+        try:
+            if sys.modules[i] is not None:
+                del (sys.modules[i])
+                if not silent:
+                    print(f"Unloaded: {i}")
+        except (Exception, ):
+            print(f"Failed to unload: {i}")
