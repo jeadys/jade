f46186663b46c8b2e7a4875fe67592c6a36f702c
Jeadys <yassin.ao.work@gmail.com>
2024-06-21 23:38:53 +0200

    blueprint component segments removed from nested function

--- modular/components/arachne_leg.py
+++ modular/components/arachne_leg.py
@@ -1,25 +1,23 @@
 from modular.biped.biped import Segment, Control
 from utilities.enums import Orient, Shape, Color
 
+arachne_thigh_control = Control(name="arachne_thigh", parent=None, shape=Shape.CUBE, color=Color.GREEN, scale=2.5)
+arachne_thigh_joint = Segment(name="arachne_thigh", parent=None, position=(5, 10, 0), orientation=Orient.BONE,
+                              control=arachne_thigh_control)
 
-def arachne_leg_segments() -> list[Segment]:
-    arachne_thigh_control = Control(name="arachne_thigh", parent=None, shape=Shape.CUBE, color=Color.GREEN, scale=2.5)
-    arachne_thigh_joint = Segment(name="arachne_thigh", parent=None, position=(5, 10, 0), orientation=Orient.BONE,
-                                  control=arachne_thigh_control)
+arachne_shin_control = Control(name="arachne_shin", parent=arachne_thigh_control, shape=Shape.CUBE,
+                               color=Color.GREEN, scale=2.5)
+arachne_shin_joint = Segment(name="arachne_shin", parent=arachne_thigh_joint, position=(10, 5, 0),
+                             orientation=Orient.BONE, control=arachne_shin_control)
 
-    arachne_shin_control = Control(name="arachne_shin", parent=arachne_thigh_control, shape=Shape.CUBE,
-                                   color=Color.GREEN, scale=2.5)
-    arachne_shin_joint = Segment(name="arachne_shin", parent=arachne_thigh_joint, position=(10, 5, 0),
-                                 orientation=Orient.BONE, control=arachne_shin_control)
+arachne_foot_control = Control(name="arachne_foot", parent=arachne_shin_control, shape=Shape.CUBE,
+                               color=Color.GREEN, scale=2.5)
+arachne_foot_joint = Segment(name="arachne_foot", parent=arachne_shin_joint, position=(2.5, -10, 0),
+                             orientation=Orient.BONE, control=arachne_foot_control)
 
-    arachne_foot_control = Control(name="arachne_foot", parent=arachne_shin_control, shape=Shape.CUBE,
-                                   color=Color.GREEN, scale=2.5)
-    arachne_foot_joint = Segment(name="arachne_foot", parent=arachne_shin_joint, position=(2.5, -10, 0),
-                                 orientation=Orient.BONE, control=arachne_foot_control)
+arachne_toe_control = Control(name="arachne_toe", parent=arachne_foot_control, shape=Shape.CUBE, color=Color.GREEN,
+                              scale=2.5)
+arachne_toe_joint = Segment(name="arachne_toe", parent=arachne_foot_joint, position=(0, -5, 0),
+                            orientation=Orient.WORLD, control=arachne_toe_control)
 
-    arachne_toe_control = Control(name="arachne_toe", parent=arachne_foot_control, shape=Shape.CUBE, color=Color.GREEN,
-                                  scale=2.5)
-    arachne_toe_joint = Segment(name="arachne_toe", parent=arachne_foot_joint, position=(0, -5, 0),
-                                orientation=Orient.WORLD, control=arachne_toe_control)
-
-    return [arachne_thigh_joint, arachne_shin_joint, arachne_foot_joint, arachne_toe_joint]
+arachne_leg_segments: list[Segment] = [arachne_thigh_joint, arachne_shin_joint, arachne_foot_joint, arachne_toe_joint]
